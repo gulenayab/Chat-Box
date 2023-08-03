2039f4e549969b5810472d42e3d93aac755a4907
Gul-e-Nayab <111217531+gulenayab@users.noreply.github.com>
2023-08-03 11:10:20 +0500

    Add files via upload

---
+++ chatBox.js
@@ -0,0 +1,7 @@
+function openChat() {
+    document.getElementById("chatbox").style.display = "block";
+}
+
+function closeChat() {
+    document.getElementById("chatbox").style.display = "none";
+}
